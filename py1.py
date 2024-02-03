d6fc3a5000605ec23cfaf40a26328ab0ca9ca4c6
Adham <iamnotadham@gmail.com>
2024-02-03 19:56:19 +0200

    Done

---
+++ py1.py
@@ -0,0 +1,21 @@
+def Calc():
+    num1 = input("enter a number: ")
+    num2 = input("enter another number: ")
+
+    method = input("Enter method : ")
+
+
+    if (method == "+") or (method == "plus"):
+        print( int(num1) + int(num2) ) 
+
+    if (method == "-") or (method == "minus"):
+        print( int(num1) + int(num2) ) 
+
+    if (method == "X") or (method == "*"):
+        print( int(num1) + int(num2) ) 
+
+    if (method == "/") or (method == "÷"):
+        print( int(num1) + int(num2) ) 
+
+Calc()
+
