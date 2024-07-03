5b195c0d4d658a6af359a42e16794bd8efd69b8e
Adham <iamnotadham@gmail.com>
2024-07-03 12:36:18 +0300

    Making the animation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
+import sys
 
 #Delay function
 # def delayedPrint(string):
 #     print(string)
 #     time.sleep(5)
+
 
 
 #Just making it more customizable
@@ -12,10 +14,28 @@ def delayedPrint(string, int):
     time.sleep(int)
 
 
+#Animation function
+def animate():
+    for i in range(1,10):
+        sys.stdout.write('\rGame is loading |')
+        time.sleep(0.1)
+        sys.stdout.write('\rGame is loading /')
+        time.sleep(0.1)
+        sys.stdout.write('\rGame is loading -')
+        time.sleep(0.1)
+        sys.stdout.write('\rGame is loading \\')
+        time.sleep(0.1)
+    sys.stdout.write('\rGame loaded successfully!')
+
+
 #Starting code
 delayedPrint("===== Welcome \"THE LOST ARTIFACT\" Game =====", 0.7)
 delayedPrint("== This game fully made py Adham ==", 1)
 print(" ")
 input("Press (Enter) to start game")
 delayedPrint("===================================", 1)
+
+
+animate()
+
 
